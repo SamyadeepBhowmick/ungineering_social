c9e48f00c5db39d3f45935358684aa4d45eaf70c
Supriya <supisingh305@gmail.com>
2019-01-09 18:12:56 +0530

    My Dashboard version3

--- dashboard_submit.php
+++ dashboard_submit.php
@@ -4,28 +4,39 @@
     $username = "root";
     $db_password = "supriya1234";
     $database = "social_media";
-
+    
+    $response = array();
     $conn = mysqli_connect($hostname, $username, $db_password, $database);
     if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
+        $response['success'] = false;
+        $response['message'] = "Connection failed: " . mysqli_connect_error();
+        echo json_encode($response);
+        exit();
     }
+    
     $id=$_SESSION['user_id']; //echo $id; 
     $college=$_POST['college']; //echo $college;
     $phone_no=$_POST['phone_no']; //echo $phone_no;
     $name=$_POST['name'];
     $password=$_POST['password'];
     $email=$_POST['email'];
+    
     $sql = "UPDATE users
       SET college='$college',phone_no= '$phone_no',name='$name',email= '$email' ,password='$password'
       WHERE user_id= $id";// echo $sql;
+      
     $result = mysqli_query($conn, $sql);
     if (!$result) {
-        die("Error: " . $sql . "<br>" . mysqli_error($conn));
+        $response['success'] = false;
+        $response['message'] = "Error: " . $sql . "<br>" . mysqli_error($conn);
+        echo json_encode($response);
+        exit();
     }    	
-    echo"update sucessful";
+    $response['success'] = true;
+    $response['message'] = "Updated";
+    echo json_encode($response);
     mysqli_close($conn);
 ?>  
-<a href="dashboard2.php">Back</a>
 
 
 
@@ -35,31 +46,4 @@
 
 
 
-<?php 
-    /*session_start(); 
-    $hostname="localhost";
-    $username="root";
-    $db_password="supriya1234";
-    $database="social_media";
-    
-    $conn=mysqli_connect($hostname,$username,$db_password,$database);
-    if(!$conn){
-        die("connection failed:" . mysqli_connect_error());
-    }
-    
-    $id=$_SESSION['user_id']; //echo $id; 
-    $college=$_POST['college']; //echo $college;
-    $phone_no=$_POST['phone_no']; //echo $phone_no;
-    
-    $sql="UPDATE users 
-          SET college='$college', phone_no='$phone_no'
-          WHERE user_id = $id";
-           
-    $result = mysqli_query($conn, $sql);     	
-    /*if(!$result)){
-        die("Error: " . $sql . "<br>" . mysqli_error($conn));
-    }
-    
-    echo"update successfull";
-    mysqli_close($conn);
-?>
+
--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,26 @@
-var update = document.getElementsByClassName("box4")[0];
+$(document).ready(function () {
+    $('#dashboard2').submit(function() {
+        var url = "dashboard_submit.php";
+        var data = $('#dashboard2').serialize();
+        $.ajax(url, {
+            data: data,
+            success: registration_success,
+            error: on_error,
+            type: "POST"
+        });
+        return false;
+    });
+});
+var registration_success = function (data) {
+    data = JSON.parse(data); 
 
-
+    if (data.success) {
+        alert(data.message);
+        window.location.href = "dashboard2.php"; 
+    } else {
+        alert(data.message);
+    }
+};
+var on_error = function () {
+    alert("something went wrong");
+};
